77c5aebed61698d677a6726c105814293890e60c
unknown <davilaeliseo453@gmail.com>
2020-11-23 22:12:05 -0600

    mi primer commit

---
+++ calculadora/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("HOLA MUNDO");
+            Console.WriteLine(" Mi nombre es eliseo davila ojeda");
+
+        }
+    }
+}
